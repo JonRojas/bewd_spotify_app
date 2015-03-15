14f51281e87851884e150f31acaab7793f2ba518
JonRojas <jonathan.rojas@gmail.com>
2015-03-15 15:59:19 -0400

    Error with @Results in controller

--- Gemfile
+++ Gemfile
@@ -45,3 +45,4 @@ end
 
 gem 'httparty'
 gem 'pry-rails'
+
--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -3,15 +3,24 @@ class SpotifyController < ApplicationController
 	def index
 		if params[:year]
 			year = params[:year]
-			response = HTTParty.get("https://api.spotify.com//v1/browse/new-releases+year=#{year}")
+			response = HTTParty.get("https://api.spotify.com/v1/search?type=playlist&q=#{year}")
 			results = JSON.parse(response.body)
-			@results = results['year']
+			@results = results['playlists.name']
 		else
 			@results = []
 		end
+
 	end
+
+
+
+
 
 	def show
-
+		response = HTTParty.get("https://api.spotify.com/v1/search?type=playlist&q=#{params[:year]}")
+		@result = JSON.parse(response.body)
 	end
 end
+
+
+
