a9f68ed6e1cda0858d35fbfaea7fc5d885389780
JonRojas <jonathan.rojas@gmail.com>
2015-03-16 18:28:20 -0400

    Show controller and view needs work :)

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -13,8 +13,11 @@ class SpotifyController < ApplicationController
 		
 
 	def show
-		response = HTTParty.get("https://api.spotify.com/v1/search?type=playlist&q=#{params[:year]}")
+		# need playlists IDs
+		year = params[:year]
+		response = HTTParty.get("https://api.spotify.com/v1/search?type=playlist&q=#{year}")
 		@result = JSON.parse(response.body)
+		binding.pry
 	end
 end
 
